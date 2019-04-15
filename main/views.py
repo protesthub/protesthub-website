9165c2671a3b49057a187802c148187beb423904
scientes <scientes12@gmail.com>
2019-04-15 22:36:23 +0200

    Implemented Searchfield

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import TemplateView
 
 from .models import Demo
+import operator
+
+from django.db.models import Q
 
 
 def index(request):
@@ -11,4 +15,24 @@ def index(request):
         "latest_demos": latest_demos,
     }
     return HttpResponse(template.render(output, request))
+
+def single_site(request):
+    single_demo=Demo.objects("-is_like")[:1]
+    template =loader._get_template("main/demo.html")
+    output = {
+        "demo": single_demo,
+    }
+    return HttpResponse(template.render(output, request))
+
+class search_view(TemplateView):
+    template_name="main/search.html"
+
+    def get(self,request,*args,**kwargs):
+        q=request.GET.get("q","q")
+        self.results=Demo.objects.filter(Q(title__icontains=q) | Q(adress__icontains=q) | Q(organizer__icontains=q) | Q(starting_time__icontains=q) | Q(ending_time__icontains=q) | Q(description__icontains=q))
+
+        return super().get(request,*args,**kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(results=self.results, **kwargs)
 # Create your views here.
