213120fc1edf1d2c261861dc336ba6741d9da610
scientes <scientes12@gmail.com>
2019-04-15 16:25:06 +0200

    Funktionierender View für Demoliste

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Demo
 
 
 def index(request):
-    return HttpResponse("Moin das hier ist das Skelett der Protesthub seite")
+    latest_demos = Demo.objects.order_by("-created_at")[:10]
+    template = loader.get_template("main/index.html")
+    output = {
+        "latest_demos": latest_demos,
+    }
+    return HttpResponse(template.render(output, request))
 # Create your views here.
