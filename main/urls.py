77528eef16a8dde6ead8204a38dd04eaa0c9de57
thacoon <thacoon@posteo.de>
2019-04-18 17:39:32 +0200

    Add SignUp view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/',views.search_view.as_view(),name='search')
+    path('search/', views.search_view.as_view(),name='search'),
+    path('users/', include('main.users.urls'))
 ]
---
+++ main/users/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def sign_up(request):
+    return HttpResponse("SignUp")
