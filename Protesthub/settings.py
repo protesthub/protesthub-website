1463450903a25f3b9e4eddbbfe8c06e6ee658140
thacoon <thacoon@posteo.de>
2019-04-27 14:05:32 +0200

    Rename Profile to Users

--- Protesthub/settings.py
+++ Protesthub/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Protesthub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -100,8 +101,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'users.Profile'
-AUTHENTICATION_BACKENDS = ['main.users.models.ProfileBackend']
+AUTH_USER_MODEL = 'users.User'
+AUTHENTICATION_BACKENDS = ['main.users.models.UserBackend']
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- main/users/models.py
+++ main/users/models.py
@@ -4,23 +4,22 @@ from django.contrib.auth.hashers import check_password
 from django.core.validators import validate_email, ValidationError
 
 
-class Profile(AbstractUser):
+class User(AbstractUser):
     """
-    Default user profile
+    Default user
     """
     email = models.EmailField('email', max_length=255, unique=True)
     terms_of_use = models.BooleanField(default=False)
     newsletter = models.BooleanField(default=False)
 
 
-class ProfileBackend:
-    def authenticate(self, request, username=None, password=None):
+class UserBackend:
+    @staticmethod
+    def authenticate(request, username=None, password=None):
         """
         Authenticates a user by email org username and password.
-        TODO: Sanitize input data and check if it is an email. This should already be checked by a form, but better we
-        TODO: have more layers of security.
         :param request:
-        :param username: An email or username. TODO If this is not called username authentication does not work?
+        :param username: An email or username. If this is not called username authentication does not work?
         :param password:
         :return: Profile is email and password is valid otherwise None.
         """
@@ -28,10 +27,10 @@ class ProfileBackend:
             try:
                 validate_email(username)
             except ValidationError:
-                user = Profile.objects.get(username=username)
+                user = User.objects.get(username=username)
             else:
-                user = Profile.objects.get(email=username)
-        except Profile.DoesNotExist:
+                user = User.objects.get(email=username)
+        except User.DoesNotExist:
             return None
         else:
             if check_password(password, user.password) is True:
@@ -39,8 +38,9 @@ class ProfileBackend:
 
         return None
 
-    def get_user(self, user_id):
+    @staticmethod
+    def get_user(user_id):
         try:
-            return Profile.objects.get(pk=user_id)
-        except Profile.DoesNotExist:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return None
